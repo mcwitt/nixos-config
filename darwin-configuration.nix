e0c9864c8b10d758385c92ac973763a63abe93b6
Matt Wittmann <mcwitt@gmail.com>
2020-01-11 13:43:55 -0700

    Set maxJobs, buildCores

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -19,8 +19,6 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  # You should generally set this to the total number of logical cores in your system.
-  # $ sysctl -n hw.ncpu
-  nix.maxJobs = 1;
-  nix.buildCores = 1;
+  nix.maxJobs = 8;
+  nix.buildCores = 0;
 }
