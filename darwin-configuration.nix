3c8c0d17c62a36ab5b76f011d9649ef44ecfaf13
Matt Wittmann <mcwitt@gmail.com>
2020-01-11 22:13:09 -0700

    Add tmux configuration

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -13,6 +13,12 @@
 
   programs.zsh.enable = true;
 
+  programs.tmux = {
+    enable = true;
+    enableSensible = true;
+    enableVim = true;
+  };
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
