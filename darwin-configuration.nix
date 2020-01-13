bd430ac78075b9fc00306c9a197645dbdd1e035e
Matt Wittmann <mcwitt@gmail.com>
2020-01-12 18:57:47 -0700

    Enable postgresql

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -19,6 +19,8 @@
     enableVim = true;
   };
 
+  services.postgresql.enable = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
