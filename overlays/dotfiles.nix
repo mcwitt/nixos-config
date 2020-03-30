8efc8297a8d0e312f683c3d13ded75fd09ce4235
Matt Wittmann <mcwitt@gmail.com>
2020-03-29 18:14:10 -0700

    Bump dotfiles

--- overlays/dotfiles.nix
+++ overlays/dotfiles.nix
@@ -2,7 +2,7 @@ self: super: {
   mcwitt-dotfiles = (super.fetchFromGitHub {
     owner = "mcwitt";
     repo = "dotfiles";
-    rev = "99d22f5178711bfeb913167a2f8fff31424ba803";
-    sha256 = "06dxk3j1mhphdk9qh6wqfmjdlc7z589cyw46wdq7myj6a8wh4x9m";
+    rev = "62d8903403b0efebc2410dd38d4bc3844c191547";
+    sha256 = "0m2l44g4a6x90b0vc3lvqby62zm7qs9przp3ghjbkvm8mf0cv4ms";
   });
 }
