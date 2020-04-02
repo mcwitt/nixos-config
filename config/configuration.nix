09c9879dfe36b846ade0a37ca3ae3f0550a54181
Matt Wittmann <mcwitt@gmail.com>
2020-04-02 08:36:11 -0700

    Revert "Update monitor config"
    This reverts commit 96a16af14818a929eba4955d93a7d75e285ace28.

--- config/configuration.nix
+++ config/configuration.nix
@@ -143,13 +143,13 @@
       xrandrHeads = [
         {
           primary = true;
-          output = "DP-2";
+          output = "DP-4";
         }
         {
           monitorConfig = ''
             Option "Rotate" "left"
           '';
-          output = "DP-4";
+          output = "DP-2";
         }
       ];
     };
