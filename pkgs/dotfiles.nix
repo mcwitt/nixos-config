6606c120428d77982655c4c938feea74596d4296
Matt Wittmann <mcwitt@gmail.com>
2020-04-27 13:09:31 -0700

    Simplify

--- pkgs/dotfiles.nix
+++ pkgs/dotfiles.nix
@@ -1,15 +1,7 @@
-{ stdenv, fetchFromGitHub }:
-stdenv.mkDerivation {
-  name = "dotfiles";
-  src = fetchFromGitHub {
-    owner = "mcwitt";
-    repo = "dotfiles";
-    rev = "7784bf564464127322fd13b03d0a8f4fb260194d";
-    sha256 = "0apx63gwhv66v6f0w2xhzh5cjlzhl3qz80ylc0wfcfczkccccvqk";
-  };
-  phases = [ "installPhase" ];
-  installPhase = ''
-    mkdir -p $out
-    cp -r $src/* $out
-  '';
+{ fetchFromGitHub }:
+fetchFromGitHub {
+  owner = "mcwitt";
+  repo = "dotfiles";
+  rev = "7784bf564464127322fd13b03d0a8f4fb260194d";
+  sha256 = "0apx63gwhv66v6f0w2xhzh5cjlzhl3qz80ylc0wfcfczkccccvqk";
 }
