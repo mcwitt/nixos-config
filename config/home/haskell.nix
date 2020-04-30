99d5481f45d81711829bfc7359d48f4de2d01f4b
Matt Wittmann <mcwitt@gmail.com>
2020-04-30 11:08:19 -0700

    Move ghcide to overlays

--- config/home/haskell.nix
+++ config/home/haskell.nix
@@ -1,12 +1,5 @@
 { pkgs, ... }:
 let
-  ghcide = import (pkgs.fetchFromGitHub {
-    owner = "cachix";
-    repo = "ghcide-nix";
-    rev = "f940ec611cc6914693874ee5e024eba921cab19e";
-    sha256 = "0vri0rivdzjvxrh6lzlwwkh8kzxsn82jp1c2w5rqzhp87y6g2k8z";
-  }) { };
-
   ghcEnv = pkgs.haskellPackages.ghcWithPackages (ps:
     with ps; [
       array
@@ -23,7 +16,7 @@ let
     ]);
 
 in {
-  home.packages = [ ghcEnv ghcide.ghcide-ghc865 ]
+  home.packages = [ ghcEnv pkgs.ghcide.ghcide-ghc865 ]
     ++ (with pkgs.haskellPackages; [
       brittany
       cabal-install
