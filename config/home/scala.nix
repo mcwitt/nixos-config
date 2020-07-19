7b104a89745c93aa148eb3f240c7a251d5161c41
Matt Wittmann <mcwitt@gmail.com>
2020-07-19 12:11:32 -0700

    Add VS Code extensions for Scala

--- config/home/scala.nix
+++ config/home/scala.nix
@@ -1,1 +1,7 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ metals sbt scalafmt ]; }
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ metals sbt scalafmt ];
+  programs.vscode.extensions = with pkgs.vscode-extensions; [
+    scala-lang.scala
+    scalameta.metals
+  ];
+}
