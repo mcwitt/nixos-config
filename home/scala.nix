a08a98df076f5d89baa53549935bcf10b7dbc451
Matt Wittmann <mcwitt@gmail.com>
2020-09-19 18:41:22 -0700

    Add javaHome to VS Code settings

--- home/scala.nix
+++ home/scala.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ metals sbt scalafmt ];
-  programs.vscode.extensions = with pkgs.vscode-extensions; [
-    scala-lang.scala
-    scalameta.metals
-  ];
+  programs.vscode = {
+    extensions = with pkgs.vscode-extensions; [
+      scala-lang.scala
+      scalameta.metals
+    ];
+    userSettings.metals.javaHome = pkgs.jdk11_headless;
+  };
 }
