a25a585f46a89d6264fde491ec8c0385aa235d39
Matt Wittmann <mcwitt@gmail.com>
2020-10-10 12:42:58 -0700

    Move xmonad config to separate repo

--- machines/golem/home.nix
+++ machines/golem/home.nix
@@ -161,24 +161,7 @@ in {
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
-
-      config = pkgs.writeText "xmonad.hs" ''
-        import XMonad
-        import XMonad.Hooks.DynamicLog
-        import XMonad.Layout.NoBorders
-
-        -- modify default layout hook with 'smartBorders'
-        myLayoutHook = smartBorders $ layoutHook def
-
-        main = xmonad =<< xmobar def
-          { borderWidth        = 5
-          , normalBorderColor  = "#073642"
-          , focusedBorderColor = "#859900"
-          , layoutHook         = myLayoutHook
-          , modMask            = mod4Mask
-          , terminal           = "alacritty"
-          }
-      '';
+      config = "${pkgs.mypkgs.sources.xmonad-config}/xmonad.hs";
     };
   };
 }
