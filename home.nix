e8ea3d29c381ab9ab911bc2ccbb8f68b4582d78d
Matt Wittmann <mcwitt@gmail.com>
2020-11-22 18:28:01 -0800

    Add completions for awscli2

--- home.nix
+++ home.nix
@@ -68,6 +68,10 @@
 
     interactiveShellInit = ''
       set fish_key_bindings fish_user_key_bindings
+
+      # completions for awscli2 (from https://github.com/aws/aws-cli/issues/1079 on 2020-11-11)
+      complete --command ${pkgs.awscli2}/bin/aws --no-files \
+        --arguments '(begin; set --local --export COMP_SHELL fish set --local --export COMP_LINE (commandline) ${pkgs.awscli2}/bin/aws_completer | sed \'s/ $//\'; end)'
     '';
   };
 
