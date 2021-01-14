5497e7a13fafd968b84a4f33dfbe2c4c0436236e
Matt Wittmann <mcwitt@gmail.com>
2021-01-14 09:19:35 -0800

    Use home.profileDirectory

--- modules/darwin/home-manager/profiles/base/default.nix
+++ modules/darwin/home-manager/profiles/base/default.nix
@@ -4,7 +4,7 @@
 
   # HACK fix for darwin/fish PATH ordering; see https://github.com/LnL7/nix-darwin/issues/122
   programs.fish.shellInit = ''
-    for p in /run/current-system/sw/bin ${config.home.homeDirectory}/.nix-profile/bin
+    for p in /run/current-system/sw/bin ${config.home.profileDirectory}/bin
         if not contains $p $fish_user_paths
             set -g fish_user_paths $p $fish_user_paths
         end
