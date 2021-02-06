f7eed7f4cbbe34f149f264167d5c6c8e61245c3a
Matt Wittmann <mcwitt@gmail.com>
2021-02-06 09:51:48 -0800

    Remove obsolete override

--- modules/common/home-manager/languages/python/default.nix
+++ modules/common/home-manager/languages/python/default.nix
@@ -15,40 +15,26 @@ in
       [ pythonEnv ];
 
 
-    programs.emacs = {
-      overrides = self: super: {
-        anaconda-mode = super.anaconda-mode.overrideAttrs (_: {
-          src = pkgs.fetchFromGitHub
-            {
-              owner = "dakra";
-              repo = "anaconda-mode";
-              rev = "810163d5a65e62d58f363e2edaa3be70e6d82e25";
-              sha256 = "13v478n35k71pz5sah807ckh641pb7hzffsjvdglsv4sc1nl61zq";
-            };
-        });
+    programs.emacs.init.usePackage = {
+      anaconda-mode = {
+        enable = true;
+        hook = [
+          "(python-mode . anaconda-mode)"
+          "(python-mode . anaconda-eldoc-mode)"
+        ];
       };
 
-      init.usePackage = {
-        anaconda-mode = {
-          enable = true;
-          hook = [
-            "(python-mode . anaconda-mode)"
-            "(python-mode . anaconda-eldoc-mode)"
-          ];
-        };
-
-        company-anaconda = {
-          enable = true;
-          config = ''
-            (add-to-list 'company-backends
-                         '(company-anaconda :with company-capf))
-          '';
-        };
-
-        dap-python.enable = true;
-
-        pyvenv.enable = true;
+      company-anaconda = {
+        enable = true;
+        config = ''
+          (add-to-list 'company-backends
+                       '(company-anaconda :with company-capf))
+        '';
       };
+
+      dap-python.enable = true;
+
+      pyvenv.enable = true;
     };
   };
 }
