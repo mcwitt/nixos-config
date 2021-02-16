01f2ed22af5a1cb587c549621e57aad808ffedf8
Matt Wittmann <mcwitt@gmail.com>
2021-02-16 08:11:09 -0800

    Clean

--- modules/common/home-manager/programs/emacs/default.nix
+++ modules/common/home-manager/programs/emacs/default.nix
@@ -1,5 +1,2 @@
-let
-  nurNoPkgs =
-    import (import ../../../../../nix/sources.nix).nur { };
-in
-{ imports = [ ./org-protocol nurNoPkgs.repos.rycee.hmModules.emacs-init ]; }
+let nurNoPkgs = import (import ../../../../../nix/sources.nix).nur { };
+in { imports = [ ./org-protocol nurNoPkgs.repos.rycee.hmModules.emacs-init ]; }
