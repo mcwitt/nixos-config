4dee69d01db829eeed90a17dfc71be6910c3f463
Matt Wittmann <mcwitt@gmail.com>
2021-02-22 18:42:42 -0800

    Clean

--- modules/common/home-manager/languages/shell/default.nix
+++ modules/common/home-manager/languages/shell/default.nix
@@ -4,10 +4,8 @@ let cfg = config.languages.shell;
 in
 {
   options.languages.shell.enable = mkEnableOption "Shell language environment";
+
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ shellcheck shfmt ];
-    programs.emacs.init.usePackage.org.config = ''
-      (require 'ob-sh)
-    '';
   };
 }
