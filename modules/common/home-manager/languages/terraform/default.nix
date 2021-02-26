0690b947d47d84006903f106209e04516369d8a3
Matt Wittmann <mcwitt@gmail.com>
2021-02-25 17:43:40 -0800

    Add terraform-ls

--- modules/common/home-manager/languages/terraform/default.nix
+++ modules/common/home-manager/languages/terraform/default.nix
@@ -4,9 +4,20 @@ let cfg = config.languages.terraform;
 in
 {
   options.languages.terraform.enable = mkEnableOption "Terraform language environment";
+
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ terraform ];
+
+    home.packages = with pkgs; [ terraform terraform-ls ];
+
     programs.emacs.init.usePackage = {
+
+      lsp-terraform = {
+        enable = true;
+        config = ''
+          (setq lsp-terraform-server "${pkgs.terraform-ls}")
+        '';
+      };
+
       terraform-mode = {
         enable = true;
         mode = [ ''"\\.tf\\'"'' ];
