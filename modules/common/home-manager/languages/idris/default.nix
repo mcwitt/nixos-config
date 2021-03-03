187c7ccb36c7c7cb790acaa1336c5d79e887959a
Matt Wittmann <mcwitt@gmail.com>
2021-03-03 08:24:30 -0800

    Revert to idris-mode

--- modules/common/home-manager/languages/idris/default.nix
+++ modules/common/home-manager/languages/idris/default.nix
@@ -10,21 +10,7 @@ in
     home.packages = [ pkgs.idris2 ];
 
     programs.emacs = {
-      overrides = self: super: {
-        idris2-mode = super.trivialBuild {
-          pname = "idris2-mode";
-          src = pkgs.fetchFromGitHub
-            {
-              owner = "redfish64";
-              repo = "idris2-mode";
-              rev = "e7cfdfce2c4e6590e697d37e99ba58aae066ef7d";
-              sha256 = "03xczm4fzqcg740wj57d1wbqpz5ckqd75k47fhx3ix18cfg4qkp8";
-            };
-          packageRequires = [ super.prop-menu ];
-        };
-      };
-
-      init.usePackage.idris2-mode = {
+      init.usePackage.idris-mode = {
         enable = true;
         mode = [ ''"\\.idr\\'"'' ];
       };
