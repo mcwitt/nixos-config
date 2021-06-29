bea12a080d61718305183b6cdbbcdb0aec25f27b
Matt Wittmann <mcwitt@gmail.com>
2021-06-29 16:15:09 -0700

    Add awscli2 fixes in overlay

--- overlays/overlays.d/pkgs/default.nix
+++ overlays/overlays.d/pkgs/default.nix
@@ -2,6 +2,8 @@ self: super:
 let inherit (self) callPackage;
 in
 {
+  awscli2 = callPackage ./awscli2 { };
+
   mcwitt = {
     gitignore = callPackage ./gitignore.nix { };
     scripts = callPackage ./scripts { };
