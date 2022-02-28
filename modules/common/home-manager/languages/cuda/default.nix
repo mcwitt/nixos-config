6c009183a261a01cba2900bc431dce595656e0cc
Matt Wittmann <mcwitt@gmail.com>
2022-02-28 07:51:30 -0800

    Use c++ snippets in cuda-mode

--- modules/common/home-manager/languages/cuda/default.nix
+++ modules/common/home-manager/languages/cuda/default.nix
@@ -20,6 +20,20 @@ in
           (add-to-list 'lsp-language-id-configuration '(cuda-mode . "cpp"))
         '';
       };
+
+      yasnippet-snippets.package = ps: ps.yasnippet-snippets.overrideAttrs (oldAttrs: {
+        patches =
+          let cudaSnippets = pkgs.writeText "yasnippet-cuda-mode.patch" ''
+            diff --git a/snippets/cuda-mode/.yas-parents b/snippets/cuda-mode/.yas-parents
+            new file mode 100644
+            index 0000000..7a0ada1
+            --- /dev/null
+            +++ b/snippets/cuda-mode/.yas-parents
+            @@ -0,0 +1 @@
+            +c++-mode
+          '';
+          in oldAttrs.patches or [ ] ++ [ cudaSnippets ];
+      });
     };
 
     programs.vscode = {
