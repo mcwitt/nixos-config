e7f421832edfce11d2a1f7fd69fccc90e366bb90
Matt Wittmann <mcwitt@gmail.com>
2022-03-05 21:21:17 -0800

    Update for api change

--- modules/common/home-manager/programs/emacs/org-protocol/default.nix
+++ modules/common/home-manager/programs/emacs/org-protocol/default.nix
@@ -5,12 +5,12 @@ let
   orgProtocolDesktopItem = pkgs.makeDesktopItem rec {
     name = "org-protocol";
     desktopName = name;
-    mimeType = "x-scheme-handler/org-protocol";
+    mimeTypes = [ "x-scheme-handler/org-protocol" ];
     exec = "${config.programs.emacs.finalPackage}/bin/emacsclient %u";
     icon = "emacs";
     type = "Application";
-    terminal = "false";
-    categories = "System";
+    terminal = false;
+    categories = [ "System" ];
   };
 in
 {
