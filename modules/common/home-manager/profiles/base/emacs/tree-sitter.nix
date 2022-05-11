f3f24a61d5e8b80856f596499189f2b022441324
Matt Wittmann <mcwitt@gmail.com>
2022-05-11 16:26:07 -0700

    Remove override for emacs tree-sitter

--- modules/common/home-manager/profiles/base/emacs/tree-sitter.nix
+++ modules/common/home-manager/profiles/base/emacs/tree-sitter.nix
@@ -1,18 +1,4 @@
-{ pkgs, ... }:
 {
-  programs.emacs.overrides =
-    let nixpkgs = import
-      (pkgs.fetchFromGitHub {
-        owner = "mcwitt";
-        repo = "nixpkgs";
-        rev = "884c2eed250306bc7523448b2b93613bbd3f1324";
-        sha256 = "sha256-GckygchPdeYwau6Gze7uncN5cIpkwkQdllbAcdKtrJE=";
-      })
-      { }; in
-    _: _: {
-      inherit (nixpkgs.emacsPackages) tsc tree-sitter-langs;
-    };
-
   programs.emacs.init.usePackage = {
 
     tree-sitter = {
