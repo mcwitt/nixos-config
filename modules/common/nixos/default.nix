f853ddebfc20d382480d5d1edf379754cf150dee
Matt Wittmann <mcwitt@gmail.com>
2022-06-05 10:20:45 -0700

    Remove unused fonts

--- modules/common/nixos/default.nix
+++ modules/common/nixos/default.nix
@@ -9,12 +9,9 @@
     enableDefaultFonts = true;
     fonts = with pkgs; [
       emacs-all-the-icons-fonts
-      fira-code
       fira-code-symbols
-      iosevka
       joypixels
       nerdfonts
-      source-code-pro
     ];
   };
 
