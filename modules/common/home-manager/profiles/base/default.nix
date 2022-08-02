5896bebe9399cd472472f79328c177f9321b9a4c
Matt Wittmann <mcwitt@gmail.com>
2022-08-01 22:37:42 -0700

    Remove obsolete scripts

--- modules/common/home-manager/profiles/base/default.nix
+++ modules/common/home-manager/profiles/base/default.nix
@@ -14,7 +14,6 @@ let sources = import ../../../../../nix/sources.nix; in
       hub
       lab
       nix-du
-      pkgs.mcwitt.scripts
       pre-commit
       proselint
       ripgrep
--- overlays/overlays.d/pkgs/default.nix
+++ overlays/overlays.d/pkgs/default.nix
@@ -3,7 +3,6 @@ self: _:
   mcwitt = {
     gitignore = self.callPackage ./gitignore.nix { };
     lib = self.callPackage ./lib.nix { };
-    scripts = self.callPackage ./scripts { };
     vscode-extensions = self.callPackage ./vscode-extensions { };
   };
 }
