37a2ddf7df9e6ac7754b9493503d00855005930a
Matt Wittmann <mcwitt@gmail.com>
2022-11-08 08:46:46 -0800

    Add vim-autoformat, format on save

--- modules/common/home-manager/profiles/base/neovim/default.nix
+++ modules/common/home-manager/profiles/base/neovim/default.nix
@@ -23,6 +23,7 @@
       syntastic
       tagbar
       vim-airline
+      vim-autoformat
       vim-fugitive
       vim-gitgutter
       vim-surround
@@ -48,6 +49,8 @@
       let g:syntastic_auto_loc_list = 1
       let g:syntastic_check_on_open = 1
       let g:syntastic_check_on_wq = 0
+
+      au BufWrite * :Autoformat
     '';
   };
 }
