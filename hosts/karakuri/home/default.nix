7144a170195baff3fcddb5cc9e4d891044a16f8a
Matt Wittmann <mcwitt@gmail.com>
2023-01-03 10:06:30 -0800

    Fix color references

--- hosts/karakuri/home/default.nix
+++ hosts/karakuri/home/default.nix
@@ -11,50 +11,49 @@ in
 
   programs.rofi.font = lib.mkForce "Iosevka Comfy 24"; # HACK since dpi setting in Xresources seems ignored
 
-  programs.xmobar.rc = let cfg = config.programs.xmobar.rc; in
-    {
-      commands = [
-        ''
-          Run Cpu     [ "--Low"      , "50"
-                      , "--High"     , "85"
-                      , "--low"      , "${cfg.colors.good}"
-                      , "--normal"   , "${cfg.colors.normal}"
-                      , "--high"     , "${cfg.colors.alert}"
-                      , "--ppad"     , "3"
-                      ] 10
-        ''
-        ''
-          Run Battery [ "--template" , "Batt: <acstatus>"
-                      , "--Low"      , "10"
-                      , "--High"     , "80"
-                      , "--low"      , "${cfg.colors.alert}"
-                      , "--normal"   , "${cfg.colors.normal}"
-                      , "--high"     , "${cfg.colors.good}"
+  programs.xmobar.rc = let s = config.scheme.withHashtag; in {
+    commands = [
+      ''
+        Run Cpu     [ "--Low"      , "50"
+                    , "--High"     , "85"
+                    , "--low"      , "${s.green}"
+                    , "--normal"   , "${s.yellow}"
+                    , "--high"     , "${s.red}"
+                    , "--ppad"     , "3"
+                    ] 10
+      ''
+      ''
+        Run Battery [ "--template" , "Batt: <acstatus>"
+                    , "--Low"      , "10"
+                    , "--High"     , "80"
+                    , "--low"      , "${s.red}"
+                    , "--normal"   , "${s.yellow}"
+                    , "--high"     , "${s.green}"
 
-                      , "--" -- battery specific options
-                      -- discharging status
-                      , "-o"  , "<left>% (<timeleft>)"
-                      -- AC "on" status
-                      , "-O"  , "<fc=${cfg.colors.normal}>Charging</fc>"
-                      -- charged status
-                      , "-i"  , "<fc=${cfg.colors.good}>Charged</fc>"
-                      ] 50
-        ''
-      ];
+                    , "--" -- battery specific options
+                    -- discharging status
+                    , "-o"  , "<left>% (<timeleft>)"
+                    -- AC "on" status
+                    , "-O"  , "<fc=${s.yellow}>Charging</fc>"
+                    -- charged status
+                    , "-i"  , "<fc=${s.green}>Charged</fc>"
+                    ] 50
+      ''
+    ];
 
-      extraConfig = {
-        font = ''"xft:Iosevka:size=12:bold:antialias=true"'';
-        position = ''
-          Static
-            { xpos = 0
-            , ypos = 0
-            , width = 3840
-            , height = ${toString statusBarHeight}
-            }
-        '';
-        template = ''"%StdinReader% | %cpu% | %memory% | %battery% | %dynnetwork% }{| %date%"'';
-      };
+    extraConfig = {
+      font = ''"xft:Iosevka:size=12:bold:antialias=true"'';
+      position = ''
+        Static
+          { xpos = 0
+          , ypos = 0
+          , width = 3840
+          , height = ${toString statusBarHeight}
+          }
+      '';
+      template = ''"%StdinReader% | %cpu% | %memory% | %battery% | %dynnetwork% }{| %date%"'';
     };
+  };
 
   services.blueman-applet.enable = true;
 
