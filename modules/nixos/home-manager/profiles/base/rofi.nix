d47ca32210aa441e4aba194e1a5d7d5a3da4a201
Matt Wittmann <mcwitt@gmail.com>
2023-03-05 16:29:54 -0800

    Add workaround for rofi-pass, clean

--- modules/nixos/home-manager/profiles/base/rofi.nix
+++ modules/nixos/home-manager/profiles/base/rofi.nix
@@ -1,6 +1,5 @@
 { config, inputs, lib, pkgs, ... }:
 {
-
   home.packages = [
     (pkgs.rofi-pass.override {
       rofi = config.programs.rofi.finalPackage;
@@ -9,26 +8,26 @@
 
   programs.rofi = {
     enable = true;
+
     font = "Iosevka Comfy 10";
 
     # This installs a copy of rofi without plugins but still using
-    # global config, leading to "missing plugin" startup errors.
-    # Work around by overriding rofi-pass to use rofi.finalPackage
+    # global config, leading to "missing plugin" startup errors. Work
+    # around by overriding rofi-pass to use rofi.finalPackage
     # pass.enable = true;
 
-    plugins = [ pkgs.rofi-calc ];
+    plugins = with pkgs; [
+      rofi-calc
+      rofi-emoji
+    ];
+
+    terminal = "wezterm";
 
     theme = config.scheme inputs.base16-rofi;
-
-    extraConfig = {
-      modi = lib.concatStringsSep "," [
-        "window"
-        "run"
-        "ssh"
-        "drun"
-        "calc"
-      ];
-      terminal = "wezterm";
-    };
   };
+
+  # https://github.com/carnager/rofi-pass/issues/226
+  xdg.configFile."rofi-pass/config".text = ''
+    help_color="${config.scheme.withHashtag.base0D}"
+  '';
 }
