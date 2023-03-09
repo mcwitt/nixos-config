79884529877c1600e53d4de76dc6547c31329e02
Matt Wittmann <mcwitt@gmail.com>
2023-03-08 21:40:26 -0800

    Remove vestigial xmonad config, simplify

--- modules/nixos/home-manager/profiles/base/xmonad.nix
+++ modules/nixos/home-manager/profiles/base/xmonad.nix
@@ -8,75 +8,35 @@
     extraPackages = ps: [ ps.dbus ];
 
     config = with config.scheme.withHashtag; pkgs.writeText "xmonad.hs" ''
-      {-# LANGUAGE ImportQualifiedPost #-}
-
-      import Codec.Binary.UTF8.String qualified as UTF8
-      import DBus qualified as D
-      import DBus.Client qualified as D
       import Data.List (isInfixOf)
       import Data.Ratio ((%))
       import XMonad
-      import XMonad.Actions.Minimize
-      import XMonad.Hooks.DynamicLog
-      import XMonad.Hooks.EwmhDesktops
-      import XMonad.Hooks.ManageDocks (ToggleStruts (ToggleStruts), avoidStruts, docks, manageDocks)
+      import XMonad.Actions.Minimize (maximizeWindowAndFocus, minimizeWindow, withLastMinimized)
+      import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
+      import XMonad.Hooks.ManageDocks (avoidStruts, docks, manageDocks)
       import XMonad.Layout.BoringWindows (boringWindows, focusDown, focusMaster, focusUp)
-      import XMonad.Layout.Minimize
+      import XMonad.Layout.Minimize (minimize)
       import XMonad.Layout.MultiToggle (Toggle (Toggle), mkToggle, single)
-      import XMonad.Layout.Reflect (REFLECTX (REFLECTX))
-      import XMonad.Layout.Spacing
-      import XMonad.Prompt
+      import XMonad.Layout.Spacing (spacingWithEdge)
       import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
-      import XMonad.Util.Run (hPutStrLn, spawnPipe)
 
-      myLayoutHook =
-        let tall = Tall 1 (1 % 50) (3 % 5)
-            layouts = tall ||| Mirror tall ||| Full
-            minimizeBoring = minimize . boringWindows
-         in spacingWithEdge 8
-              . avoidStruts
-              . mkToggle (single REFLECTX)
-              . minimizeBoring
-              $ layouts
-
-      myPromptConfig =
-        def
-          { position = Top,
-            promptBorderWidth = 0,
-            defaultText = "",
-            alwaysHighlight = True,
-            height = 36,
-            font = "xft:Iosevka Comfy:size=10:bold:antialias=true",
-            searchPredicate = isInfixOf
-          }
-
-      myLogHook :: D.Client -> PP
-      myLogHook dbus = def {ppOutput = dbusOutput dbus}
-
-      dbusOutput :: D.Client -> String -> IO ()
-      dbusOutput dbus str = do
-        let signal = (D.signal objectPath interfaceName memberName) {D.signalBody = [D.toVariant $ UTF8.decodeString str]}
-        D.emit dbus signal
-        where
-          objectPath = D.objectPath_ "/org/xmonad/Log"
-          interfaceName = D.interfaceName_ "org.xmonad.Log"
-          memberName = D.memberName_ "Update"
-
-      main = do
-        dbus <- D.connectSession
-        D.requestName
-          dbus
-          (D.busName_ "org.xmonad.Log")
-          [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+      main =
         xmonad
           . docks
+          . ewmhFullscreen
           . ewmh
           $ def
             { borderWidth = 5,
               normalBorderColor = "${base00}",
               focusedBorderColor = "${base0D}",
-              layoutHook = myLayoutHook,
-              logHook = dynamicLogWithPP (myLogHook dbus),
+              layoutHook =
+                let tall = Tall 1 (1 % 50) (3 % 5)
+                    layouts = tall ||| Mirror tall ||| Full
+                 in spacingWithEdge 8
+                      . avoidStruts
+                      . minimize
+                      . boringWindows
+                      $ layouts,
               manageHook = manageDocks,
               modMask = mod4Mask,
               terminal = "wezterm"
@@ -86,8 +46,6 @@
                                ((mod4Mask, xK_m), focusMaster),
                                ((mod4Mask, xK_backslash), withFocused minimizeWindow),
                                ((mod4Mask + shiftMask, xK_backslash), withLastMinimized maximizeWindowAndFocus),
-                               ((mod4Mask, xK_f), sendMessage $ Toggle REFLECTX),
-                               ((mod4Mask, xK_b), sendMessage ToggleStruts),
                                ((mod4Mask, xK_g), spawn "rofi -show-icons -show window"),
                                ((mod4Mask, xK_p), spawn "rofi -show-icons -show drun"),
                                ((mod4Mask + shiftMask, xK_p), spawn "rofi -show run"),
@@ -99,12 +57,12 @@
                                ((mod4Mask, xK_u), spawn "chromium-browser"),
                                ((mod4Mask, xK_s), spawn "dm-tool switch-to-greeter")
                              ]
-              `additionalKeysP` [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 2"),
-                                  ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 2"),
-                                  ("<XF86AudioMute>", spawn "amixer -q set Master toggle"),
-                                  ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 2%-"),
-                                  ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 2%+")
-                                ]
+            `additionalKeysP` [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 2"),
+                                ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 2"),
+                                ("<XF86AudioMute>", spawn "amixer -q set Master toggle"),
+                                ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 2%-"),
+                                ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 2%+")
+                              ]
     '';
   };
 }
