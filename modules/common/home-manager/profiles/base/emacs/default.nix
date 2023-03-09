d7b6870dd39034992a0ecebfbd69e25303e9a776
Matt Wittmann <mcwitt@gmail.com>
2023-03-08 22:43:45 -0800

    Show diagnostics in eglot-managed eldoc buffer

--- modules/common/home-manager/profiles/base/emacs/default.nix
+++ modules/common/home-manager/profiles/base/emacs/default.nix
@@ -345,6 +345,16 @@
          (lambda ()
            (add-hook 'flymake-diagnostic-functions 'eglot-flymake-backend nil t)
            (flymake-mode)))
+
+        ;; https://github.com/joaotavora/eglot/discussions/898
+        (add-hook 'eglot-managed-mode-hook
+                (lambda ()
+                    ;; Show flymake diagnostics first.
+                    (setq eldoc-documentation-functions
+                        (cons #'flymake-eldoc-function
+                                (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+                    ;; Show all eldoc feedback.
+                (setq eldoc-documentation-strategy #'eldoc-documentation-compose)))
       '';
     };
 
