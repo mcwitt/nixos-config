c5d287719317277c56bb84ce97d62b7a3edde2a9
Matt Wittmann <mcwitt@gmail.com>
2023-03-10 10:09:55 -0800

    Tweaks

--- modules/nixos/home-manager/profiles/base/xmonad.nix
+++ modules/nixos/home-manager/profiles/base/xmonad.nix
@@ -8,8 +8,8 @@
     extraPackages = ps: [ ps.dbus ];
 
     config = with config.scheme.withHashtag; pkgs.writeText "xmonad.hs" ''
-      import Data.List (isInfixOf)
       import Data.Ratio ((%))
+      import Graphics.X11.ExtraTypes.XF86
       import XMonad
       import XMonad.Actions.Minimize (maximizeWindowAndFocus, minimizeWindow, withLastMinimized)
       import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
@@ -18,7 +18,7 @@
       import XMonad.Layout.Minimize (minimize)
       import XMonad.Layout.MultiToggle (Toggle (Toggle), mkToggle, single)
       import XMonad.Layout.Spacing (spacingWithEdge)
-      import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+      import XMonad.Util.EZConfig (additionalKeys)
 
       main =
         xmonad
@@ -41,28 +41,30 @@
               modMask = mod4Mask,
               terminal = "wezterm"
             }
-            `additionalKeys` [ ((mod4Mask, xK_j), focusDown),
-                               ((mod4Mask, xK_k), focusUp),
-                               ((mod4Mask, xK_m), focusMaster),
-                               ((mod4Mask, xK_backslash), withFocused minimizeWindow),
-                               ((mod4Mask + shiftMask, xK_backslash), withLastMinimized maximizeWindowAndFocus),
-                               ((mod4Mask, xK_g), spawn "rofi -show-icons -show window"),
-                               ((mod4Mask, xK_p), spawn "rofi -show-icons -show drun"),
-                               ((mod4Mask + shiftMask, xK_p), spawn "rofi -show run"),
-                               ((mod4Mask, xK_o), spawn "rofi-pass"),
-                               ((mod4Mask, xK_i), spawn "rofi -show ssh"),
-                               ((mod4Mask, xK_semicolon), spawn "rofi -show emoji"),
-                               ((mod4Mask, xK_quoteright), spawn "rofi -show calc"),
-                               ((mod4Mask, xK_y), spawn "emacsclient -c -n -e '(switch-to-buffer nil)'"),
-                               ((mod4Mask, xK_u), spawn "chromium-browser"),
-                               ((mod4Mask, xK_s), spawn "dm-tool switch-to-greeter")
-                             ]
-            `additionalKeysP` [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 2"),
-                                ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 2"),
-                                ("<XF86AudioMute>", spawn "amixer -q set Master toggle"),
-                                ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 2%-"),
-                                ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 2%+")
-                              ]
+            `additionalKeys` myAdditionalKeys
+        where
+          myAdditionalKeys =
+            [ ((mod4Mask, xK_j), focusDown),
+              ((mod4Mask, xK_k), focusUp),
+              ((mod4Mask, xK_m), focusMaster),
+              ((mod4Mask, xK_backslash), withFocused minimizeWindow),
+              ((mod4Mask + shiftMask, xK_backslash), withLastMinimized maximizeWindowAndFocus),
+              ((mod4Mask, xK_g), spawn "rofi -show-icons -show window"),
+              ((mod4Mask, xK_p), spawn "rofi -show-icons -show drun"),
+              ((mod4Mask + shiftMask, xK_p), spawn "rofi -show run"),
+              ((mod4Mask, xK_o), spawn "rofi-pass"),
+              ((mod4Mask, xK_i), spawn "rofi -show ssh"),
+              ((mod4Mask, xK_semicolon), spawn "rofi -show emoji"),
+              ((mod4Mask, xK_quoteright), spawn "rofi -show calc -no-show-match -no-sort"),
+              ((mod4Mask, xK_y), spawn "emacsclient -c -n -e '(switch-to-buffer nil)'"),
+              ((mod4Mask, xK_u), spawn "chromium-browser"),
+              ((mod4Mask, xK_s), spawn "dm-tool switch-to-greeter"),
+              ((noModMask, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 2"),
+              ((noModMask, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 2"),
+              ((noModMask, xF86XK_AudioMute), spawn "amixer -q set Master toggle"),
+              ((noModMask, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 2%-"),
+              ((noModMask, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 2%+")
+            ]
     '';
   };
 }
