7f55494324cdccedbf10481786bfc3f24cad40d0
Matt Wittmann <mcwitt@gmail.com>
2023-03-28 08:19:57 -0700

    Sync consult config with example

--- modules/common/home-manager/profiles/base/emacs/default.nix
+++ modules/common/home-manager/profiles/base/emacs/default.nix
@@ -196,34 +196,59 @@
       enable = true;
 
       bind = {
+        "C-c M-x" = "consult-mode-command";
         "C-c h" = "consult-history";
-        "C-c m" = "consult-mode-command";
-        "C-c b" = "consult-bookmark";
         "C-c k" = "consult-kmacro";
-        "C-x M-:" = "consult-complex-command";
-        "C-x b" = "consult-buffer";
-        "C-x 4 b" = "consult-buffer-other-window";
-        "C-x 5 b" = "consult-buffer-other-frame";
-        "M-g M-g" = "consult-goto-line";
-        "M-g o" = "consult-outline";
+        "C-c m" = "consult-man";
+        "C-c i" = "consult-info";
+        # "[remap Info-search]" = "consult-info";
+
+        "C-x M-:" = "consult-complex-command"; # orig. repeat-complex-command
+        "C-x b" = "consult-buffer"; # orig. switch-to-buffer
+        "C-x 4 b" = "consult-buffer-other-window"; # orig. switch-to-buffer-other-window
+        "C-x 5 b" = "consult-buffer-other-frame"; # orig. switch-to-buffer-other-frame
+        "C-x r b" = "consult-bookmark"; # orig. bookmark-jump
+        "C-x p b" = "consult-project-buffer"; # orig. project-switch-to-buffer
+
+        "M-#" = "consult-register-load";
+        "M-'" = "consult-register-store"; # orig. abbrev-prefix-mark (unrelated)
+        "C-M-#" = "consult-register";
+
+        "M-y" = "consult-yank-pop"; # orig. yank-pop
+
+        "M-g e" = "consult-compile-error";
+        "M-g f" = "consult-flymake"; # Alternative: consult-flycheck
+        "M-g g" = "consult-goto-line"; # orig. goto-line
+        "M-g M-g" = "consult-goto-line"; # orig. goto-line
+        "M-g o" = "consult-outline"; # Alternative: consult-org-heading
         "M-g m" = "consult-mark";
         "M-g k" = "consult-global-mark";
-        "M-s f" = "consult-find";
-        "M-s L" = "consult-locate";
+        "M-g i" = "consult-imenu";
+        "M-g I" = "consult-imenu-multi";
+
+        "M-s d" = "consult-find";
+        "M-s D" = "consult-locate";
         "M-s g" = "consult-grep";
         "M-s G" = "consult-git-grep";
-        "M-s s" = "consult-ripgrep";
+        "M-s r" = "consult-ripgrep";
         "M-s l" = "consult-line";
-        "M-s m" = "consult-multi-occur";
+        "M-s L" = "consult-line-multi";
         "M-s k" = "consult-keep-lines";
         "M-s u" = "consult-focus-lines";
+
         "M-s e" = "consult-isearch-history";
       };
 
       bindLocal.isearch-mode-map = {
-        "M-e" = "consult-isearch-history";
-        "M-s e" = "consult-isearch-history";
-        "M-s l" = "consult-line";
+        "M-e" = "consult-isearch-history"; # orig. isearch-edit-string
+        "M-s e" = "consult-isearch-history"; # orig. isearch-edit-string
+        "M-s l" = "consult-line"; # needed by consult-line to detect isearch
+        "M-s L" = "consult-line-multi"; # needed by consult-line to detect isearch
+      };
+
+      bindLocal.minibuffer-local-map = {
+        "M-s" = "consult-history"; # orig. next-matching-history-element
+        "M-r" = "consult-history"; # orig. previous-matching-history-element
       };
 
       hook = [ "(completion-list-mode . consult-preview-at-point-mode)" ];
@@ -232,7 +257,7 @@
         ;; Optionally configure the register formatting. This improves the register
         ;; preview for `consult-register', `consult-register-load',
         ;; `consult-register-store' and the Emacs built-ins.
-        (setq register-preview-delay 0
+        (setq register-preview-delay 0.5
               register-preview-function #'consult-register-format)
 
         ;; Optionally tweak the register preview window.
@@ -245,40 +270,24 @@
       '';
 
       config = ''
-        ;; Optionally configure preview. The default value
-        ;; is 'any, such that any key triggers the preview.
-        ;; (setq consult-preview-key 'any)
-        ;; (setq consult-preview-key (kbd "M-."))
-        ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
         ;; For some commands and buffer sources it is useful to configure the
         ;; :preview-key on a per-command basis using the `consult-customize' macro.
         (consult-customize
-        consult-theme :preview-key '(:debounce 0.2 any)
-        consult-ripgrep consult-git-grep consult-grep
-        consult-bookmark consult-recent-file consult-xref
-        consult--source-bookmark consult--source-file-register
-        consult--source-recent-file consult--source-project-recent-file
-        ;; :preview-key (kbd "M-.")
-        :preview-key '(:debounce 0.4 any))
+         consult-theme :preview-key '(:debounce 0.2 any)
+         consult-ripgrep consult-git-grep consult-grep
+         consult-bookmark consult-recent-file consult-xref
+         consult--source-bookmark consult--source-file-register
+         consult--source-recent-file consult--source-project-recent-file
+         ;; :preview-key "M-."
+         :preview-key '(:debounce 0.4 any))
 
         ;; Optionally configure the narrowing key.
         ;; Both < and C-+ work reasonably well.
-        (setq consult-narrow-key "<") ;; (kbd "C-+")
+        (setq consult-narrow-key "<") ;; "C-+"
 
         ;; Optionally make narrowing help available in the minibuffer.
         ;; You may want to use `embark-prefix-help-command' or which-key instead.
-        ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-        ;; Optionally configure a function which returns the project root directory.
-        ;; There are multiple reasonable alternatives to chose from.
-        (setq consult-project-root-function
-              (lambda ()
-                (when-let (project (project-current))
-                  (car (project-roots project)))))
-
-        (setq consult-ripgrep-command (string-join '("rg" "--null" "--line-buffered" "--color=ansi"
-                                                     "--max-columns=1000"  "--no-heading" "--line-number"
-                                                     "--smart-case" "." "-e" "ARG" "OPTS") " "))
+        (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
       '';
     };
 
