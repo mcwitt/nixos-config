d59a13f9c44d021e042f25d115f950d8474909cc
Matt Wittmann <mcwitt@gmail.com>
2023-04-17 18:39:21 -0700

    Replace custom lib function with genAttrs

--- lib.nix
+++ lib.nix
@@ -3,9 +3,4 @@ final: _: {
   gitignores = path:
     final.splitString "\n"
       (builtins.readFile "${inputs.gitignore}/${path}.gitignore");
-
-  setAll = value: keys: builtins.listToAttrs
-    (map
-      (key: final.nameValuePair key value)
-      keys);
 }
