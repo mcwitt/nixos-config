b2e8f3ec15f9d289213a1161f429ddfe9b74da9e
Matt Wittmann <mcwitt@gmail.com>
2023-04-29 11:54:37 -0700

    Add cape completion extensions

--- modules/common/home-manager/profiles/base/emacs/completion/corfu.nix
+++ modules/common/home-manager/profiles/base/emacs/completion/corfu.nix
@@ -15,6 +15,26 @@
       (setq tab-always-indent 'complete))
     '';
 
+    cape = {
+      enable = true;
+      init = ''
+        ;; Add `completion-at-point-functions', used by `completion-at-point'.
+        ;; NOTE: The order matters!
+        (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+        (add-to-list 'completion-at-point-functions #'cape-file)
+        (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+        ;;(add-to-list 'completion-at-point-functions #'cape-history)
+        ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+        ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+        ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+        ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+        ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+        ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+        ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+        ;;(add-to-list 'completion-at-point-functions #'cape-line)
+      '';
+    };
+
     corfu = {
       enable = true;
       init = ''
