c239103aae46ce9c3a457006b98bad9ea473e058
Matt Wittmann <mcwitt@gmail.com>
2023-05-15 16:55:55 -0700

    Add suggested bindings for cape

--- modules/common/home-manager/profiles/base/emacs/completion/corfu.nix
+++ modules/common/home-manager/profiles/base/emacs/completion/corfu.nix
@@ -17,6 +17,7 @@
 
     cape = {
       enable = true;
+
       init = ''
         ;; Add `completion-at-point-functions', used by `completion-at-point'.
         ;; NOTE: The order matters!
@@ -33,6 +34,22 @@
         ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
         ;;(add-to-list 'completion-at-point-functions #'cape-line)
       '';
+
+      bind = {
+        "C-c p p" = "completion-at-point"; # capf
+        "C-c p t" = "complete-tag"; # etags
+        "C-c p d" = "cape-dabbrev"; # or dabbrev-completion
+        "C-c p h" = "cape-history";
+        "C-c p f" = "cape-file";
+        "C-c p k" = "cape-keyword";
+        "C-c p s" = "cape-symbol";
+        "C-c p a" = "cape-abbrev";
+        "C-c p l" = "cape-line";
+        "C-c p w" = "cape-dict";
+        "C-c p \\\\" = "cape-tex";
+        "C-c p &" = "cape-sgml";
+        "C-c p r" = "cape-rfc1345";
+      };
     };
 
     corfu = {
