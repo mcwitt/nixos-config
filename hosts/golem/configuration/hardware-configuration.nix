22b753df13e28c144fd2a94d303bc7284e08a3a1
Matt Wittmann <mcwitt@gmail.com>
2023-05-15 12:19:53 -0700

    Update hardware config

--- hosts/golem/configuration/hardware-configuration.nix
+++ hosts/golem/configuration/hardware-configuration.nix
@@ -8,23 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ad90684d-1b2b-45d3-8fc0-91854da78a80";
+    { device = "/dev/disk/by-uuid/2c94d7ca-95ae-4077-be8b-04111207ca6b";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/BCDA-0042";
+    { device = "/dev/disk/by-uuid/7D39-D03D";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/18b5a607-6d43-475e-a3ae-d3e5e06ad092"; }
+    [ { device = "/dev/disk/by-uuid/a1a91d4f-a9c3-4644-bdd2-84f3206eb0ea"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -32,9 +32,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
