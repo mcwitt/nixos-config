7604c972144125fec2db62b572a4c1ff5e537c71
Matt Wittmann <mcwitt@gmail.com>
2023-08-23 16:20:14 -0700

    Override pyflume to match home-assistant version

--- hosts/hestia/home-assistant.nix
+++ hosts/hestia/home-assistant.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   services.home-assistant = {
     config = {
@@ -23,5 +24,20 @@
     ];
 
     openFirewall = true;
+
+    package = pkgs.home-assistant.override {
+      packageOverrides = final: prev: {
+        pyflume = prev.pyflume.overridePythonAttrs (oldAttrs: rec {
+          version = "0.6.5"; # version pinned by home-assistant
+          name = "${oldAttrs.pname}-${version}";
+          src = pkgs.fetchFromGitHub {
+            owner = "ChrisMandich";
+            repo = "PyFlume";
+            rev = "v${version}";
+            hash = "sha256-kIE3y/qlsO9Y1MjEQcX0pfaBeIzCCHk4f1Xa215BBHo=";
+          };
+        });
+      };
+    };
   };
 }
