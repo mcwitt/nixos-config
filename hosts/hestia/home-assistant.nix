b4e50654052ec298dd1444c75bd5e62324b02c49
Matt Wittmann <mcwitt@gmail.com>
2023-08-22 18:41:40 -0700

    Add basic home-assistant config

---
+++ hosts/hestia/home-assistant.nix
@@ -0,0 +1,23 @@
+{
+  services.home-assistant = {
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = { };
+      homeassistant = {
+        latitude = 37.8715;
+        longitude = -122.2730;
+        name = "Home";
+      };
+    };
+
+    extraComponents = [
+      # Components required to complete the onboarding
+      "esphome"
+      "met"
+      "radio_browser"
+    ];
+
+    openFirewall = true;
+  };
+}
