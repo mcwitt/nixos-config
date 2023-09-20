e2c6dee845e0dcdae1e6f87be7849282974dd9bc
Matt Wittmann <mcwitt@gmail.com>
2023-09-19 22:14:16 -0700

    Add wake-up light automation

---
+++ hosts/hestia/configuration/home-assistant/wake-up-light.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+let inherit (config.services.home-assistant) configDir;
+in
+{
+  options.services.home-assistant.wakeUpLight.path = lib.mkOption {
+    type = lib.types.path;
+    description = "Path to link blueprint yaml";
+    default = "${configDir}/blueprints/automation/sbyx/wake-up-light-alarm-with-sunrise-effect.yaml";
+  };
+
+  config = let cfg = config.services.home-assistant.wakeUpLight; in
+    {
+      services.home-assistant.config = {
+        sensor = [
+          {
+            platform = "time_date";
+            display_options = [
+              "date"
+              "date_time_iso"
+            ];
+          }
+        ];
+      };
+
+      systemd.services.home-assistant.preStart =
+        let
+          src = pkgs.fetchurl {
+            url = "https://gist.githubusercontent.com/sbyx/96c43b13b90ae1c35b872313ba1d2d2d/raw/fc5dba10a35b882b74f42b6209d60e0084368212/wake-up-light-alarm-with-sunrise-effect.yaml";
+            hash = "sha256-OtNoV20TlN2qP0FkpRTU7i7hoSHthY9BZZUhSnMq+W8=";
+          };
+        in
+        ''
+          mkdir -p ${builtins.dirOf cfg.path}
+          rm -f ${cfg.path}
+          ln -s ${src} ${cfg.path}
+        '';
+    };
+}
