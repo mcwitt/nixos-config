d93f94eb0db1dee2076bb031d1c995ce08572f2c
Matt Wittmann <mcwitt@gmail.com>
2023-10-06 06:33:56 -0700

    Remove redundant config

--- hosts/hestia/configuration/zigbee2mqtt.nix
+++ hosts/hestia/configuration/zigbee2mqtt.nix
@@ -4,17 +4,8 @@
 
   services.zigbee2mqtt = {
     enable = true;
-
     settings = {
-      homeassistant = config.services.home-assistant.enable;
-      permit_join = false;
       frontend.port = 8080;
-
-      mqtt = {
-        base_topic = "zigbee2mqtt";
-        server = "mqtt://localhost:1883";
-      };
-
       serial.port = "/dev/serial/by-id/usb-ITEAD_SONOFF_Zigbee_3.0_USB_Dongle_Plus_V2_20230602161357-if00";
     };
   };
