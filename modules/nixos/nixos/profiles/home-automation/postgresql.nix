539d0c6d5c1f2166112e60d777bcdf15730d0c16
Matt Wittmann <mcwitt@gmail.com>
2023-11-10 09:50:07 -0800

    Downgrade postgresql to fix permissions issue

--- modules/nixos/nixos/profiles/home-automation/postgresql.nix
+++ modules/nixos/nixos/profiles/home-automation/postgresql.nix
@@ -1,12 +1,17 @@
 # https://nixos.wiki/wiki/Home_Assistant#Using_PostgreSQL
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   config = lib.mkIf config.profiles.home-automation.enable {
     services.home-assistant.config.recorder.db_url = "postgresql://@/hass";
 
     services.postgresql = {
       enable = true;
+
+      # https://github.com/NixOS/nixpkgs/issues/216989
+      package = pkgs.postgresql_14;
+
       ensureDatabases = [ "hass" ];
+
       ensureUsers = [{
         name = "hass";
         ensurePermissions = {
