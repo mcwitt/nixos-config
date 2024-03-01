7c8fd9e08b20f27a08af67e56c73f17e5b53ed9b
Matt Wittmann <mcwitt@gmail.com>
2024-03-01 09:00:12 -0800

    Switch to openjdk

--- modules/common/home-manager/languages/plantuml/default.nix
+++ modules/common/home-manager/languages/plantuml/default.nix
@@ -5,7 +5,7 @@ in
 {
   options.languages.plantuml.enable = mkEnableOption "PlantUML language environment";
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ adoptopenjdk-bin plantuml ];
+    home.packages = with pkgs; [ openjdk plantuml ];
 
     programs.emacs.init.usePackage = {
 
