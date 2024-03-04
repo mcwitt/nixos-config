dc395d7542364624cb36fed584ad1325523e126f
Matt Wittmann <mcwitt@gmail.com>
2024-03-04 08:35:10 -0800

    Update steam settings

--- modules/nixos/nixos/profiles/personal/default.nix
+++ modules/nixos/nixos/profiles/personal/default.nix
@@ -1,6 +1,10 @@
 { config, lib, ... }:
 {
   config = lib.mkIf config.profiles.personal.enable {
-    programs.steam.enable = true;
+    programs.steam = {
+      enable = true;
+      dedicatedServer.openFirewall = true;
+      remotePlay.openFirewall = true;
+    };
   };
 }
