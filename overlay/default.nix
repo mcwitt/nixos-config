566b64bea63d2ae7ebf7015e0f9f23fa8045bd69
Matt Wittmann <mcwitt@gmail.com>
2024-06-30 11:29:37 -0700

    Update cfdyndns via overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,6 +1,6 @@
 { inputs }: final: prev:
 let
-  inherit (final) callPackage lib;
+  inherit (final) callPackage fetchFromGitHub lib;
 
   assertNotStale = overrideVersion: currentVersion:
     let inherit (lib) assertMsg versionOlder; in
@@ -16,6 +16,18 @@ in
   base16-rofi = callPackage ../packages/data/themes/base16-rofi.nix { };
 
   base16-tmux = callPackage ../packages/data/themes/base16-tmux.nix { };
+
+  cfdyndns =
+    let
+      nixpkgs = fetchFromGitHub {
+        owner = "mcwitt";
+        repo = "nixpkgs";
+        rev = "068def2ae6c29ab4a5085f81df1abcdcfb906907";
+        hash = "sha256-K6zi7qJHMM4povtbQIiX7u/WY/rS+a8z/3GBRY3KRIQ=";
+      };
+      pkg = callPackage "${nixpkgs}/pkgs/applications/networking/dyndns/cfdyndns" { };
+    in
+    assert assertNotStale pkg.version prev.cfdyndns.version; pkg;
 
   fish-kubectl-completions = callPackage ../packages/shells/fish/fish-kubectl-completions.nix { };
 
