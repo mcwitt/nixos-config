5ac2a63b3cd67878b46ae220c9b0a2b9e304d128
Matt Wittmann <mcwitt@gmail.com>
2024-07-26 21:29:48 -0700

    Remove redundant override

--- overlay/default.nix
+++ overlay/default.nix
@@ -21,18 +21,6 @@ in
 
   base16-tmux = callPackage ../packages/data/themes/base16-tmux.nix { };
 
-  cfdyndns =
-    let
-      nixpkgs = fetchFromGitHub {
-        owner = "mcwitt";
-        repo = "nixpkgs";
-        rev = "068def2ae6c29ab4a5085f81df1abcdcfb906907";
-        hash = "sha256-K6zi7qJHMM4povtbQIiX7u/WY/rS+a8z/3GBRY3KRIQ=";
-      };
-      pkg = callPackage "${nixpkgs}/pkgs/applications/networking/dyndns/cfdyndns" { };
-    in
-    assert assertNotStale pkg.version prev.cfdyndns.version; pkg;
-
   fish-kubectl-completions = callPackage ../packages/shells/fish/fish-kubectl-completions.nix { };
 
   fzf-fish = callPackage ../packages/shells/fish/fzf-fish.nix { };
