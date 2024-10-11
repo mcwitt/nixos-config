23ef81891a7bd3418425d172bdcd172a37a4bbab
Matt Wittmann <mcwitt@gmail.com>
2024-10-11 11:51:56 -0700

    Merge submodule

--- modules/common/home-manager/profiles/base/emacs/default.nix
+++ modules/common/home-manager/profiles/base/emacs/default.nix
@@ -8,7 +8,6 @@
     ./forge.nix
     ./format-all.nix
     ./jupyter.nix
-    ./lsp.nix
     ./org.nix
     ./theme
     ./vertico.nix
@@ -260,6 +259,28 @@
     csv-mode.enable = true;
 
     edit-indirect.enable = true;
+
+    eglot = {
+      enable = true;
+      command = [ "eglot" ];
+
+      bindLocal.eglot-mode-map = {
+        "C-c l a" = "eglot-code-actions";
+        "C-c l r" = "eglot-rename";
+      };
+
+      config = ''
+        ;; https://github.com/joaotavora/eglot/discussions/898
+        (add-hook 'eglot-managed-mode-hook
+                  (lambda ()
+                    ;; Show flymake diagnostics first.
+                    (setq eldoc-documentation-functions
+                          (cons #'flymake-eldoc-function
+                                (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+                    ;; Show all eldoc feedback.
+                    (setq eldoc-documentation-strategy #'eldoc-documentation-compose)))
+      '';
+    };
 
     eldoc = {
       enable = true;
