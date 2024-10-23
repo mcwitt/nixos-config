f9e13448d8a1db3e29444867fa0a23d8819a89b3
Matt Wittmann <mcwitt@gmail.com>
2024-10-23 12:07:54 -0700

    Update zigbee2mqtt adapter config

--- modules/nixos/nixos/profiles/home-automation/zigbee2mqtt.nix
+++ modules/nixos/nixos/profiles/home-automation/zigbee2mqtt.nix
@@ -10,8 +10,12 @@
       settings = {
         homeassistant = config.services.home-assistant.enable;
         permit_join = true;
-        serial.port = "/dev/serial/by-id/usb-ITEAD_SONOFF_Zigbee_3.0_USB_Dongle_Plus_V2_20230602161357-if00";
         frontend.port = lib.mkDefault 8080;
+
+        serial = {
+          port = "/dev/serial/by-id/usb-ITEAD_SONOFF_Zigbee_3.0_USB_Dongle_Plus_V2_20230602161357-if00";
+          adapter = "ember";
+        };
 
         # https://www.zigbee2mqtt.io/guide/configuration/device-availability.html#availability-advanced-configuration
         availability = {
