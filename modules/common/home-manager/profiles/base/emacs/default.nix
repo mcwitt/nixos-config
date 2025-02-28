a03cf0a6ac46495c68d4d36d2e8c274b176a50d4
Matt Wittmann <mcwitt@gmail.com>
2025-02-27 20:41:27 -0800

    emacs: remove which-key

--- modules/common/home-manager/profiles/base/emacs/default.nix
+++ modules/common/home-manager/profiles/base/emacs/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   nurNoPkgs,
   pkgs,
   ...
@@ -628,18 +627,6 @@
       };
       config = ''
         (setq vundo-glyph-alist vundo-unicode-symbols)
-      '';
-    };
-
-    which-key = {
-      enable = true;
-      diminish = [ "which-key-mode" ];
-      init = ''
-        (setq which-key-separator " ")
-        (setq which-key-prefix-prefix "+")
-      '';
-      config = ''
-        (which-key-mode 1)
       '';
     };
 
