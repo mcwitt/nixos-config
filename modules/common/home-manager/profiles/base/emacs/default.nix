78bcd6c5508ca96800498baddf8e7745ccc2c3cc
Matt Wittmann <mcwitt@gmail.com>
2025-03-04 11:48:01 -0800

    emacs: add org-roam-dailies

--- modules/common/home-manager/profiles/base/emacs/default.nix
+++ modules/common/home-manager/profiles/base/emacs/default.nix
@@ -527,6 +527,14 @@
           (progn
             (make-directory d t)
             (setq org-roam-directory (file-truename d))))
+
+        (setq org-roam-dailies-directory "daily/")
+
+        (setq org-roam-dailies-capture-templates
+              '(("d" "default" entry
+                 "* %?"
+                 :target (file+head "%<%Y-%m-%d>.org"
+                                    "#+title: %<%Y-%m-%d>\n"))))
       '';
       config = ''
         (org-roam-db-autosync-mode)
