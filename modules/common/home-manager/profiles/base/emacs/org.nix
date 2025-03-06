c2927752396bd3d71bc54b7154e3cafba6fd6968
Matt Wittmann <mcwitt@gmail.com>
2025-03-05 16:14:42 -0800

    emacs: configure org

--- modules/common/home-manager/profiles/base/emacs/org.nix
+++ modules/common/home-manager/profiles/base/emacs/org.nix
@@ -1,4 +1,8 @@
 { config, lib, ... }:
+let
+  orgDir = "${config.home.homeDirectory}/src/notes";
+  orgRoamDir = "${orgDir}/org-roam";
+in
 {
   config = lib.mkIf config.profiles.base.enable {
 
@@ -12,20 +16,25 @@
         enable = true;
         bind = {
           # Standard global bindings
-          # https://orgmode.org/manual/Activation.html#Activation-1
+          # [[info:org#Activation][org#Activation]]
           "C-c l" = "org-store-link";
           "C-c a" = "org-agenda";
           "C-c c" = "org-capture";
         };
+        config = ''
+          (setq org-directory "${orgDir}")
+          (setq org-agenda-files '("${orgDir}/gtd.org"))
+
+          ;; Add +PROJECT to default stuck projects definition
+          (setq org-stuck-projects '("+LEVEL=2+PROJECT/-DONE" ("TODO" "NEXT" "NEXTACTION") nil ""))
+        '';
       };
 
       org-roam = {
         enable = true;
         init = ''
-          (let ((d "~/src/notes/org-roam"))
-            (progn
-              (make-directory d t)
-              (setq org-roam-directory (file-truename d))))
+          (make-directory "${orgRoamDir}" t)
+          (setq org-roam-directory "${orgRoamDir}")
 
           (setq org-roam-dailies-directory "daily/")
 
