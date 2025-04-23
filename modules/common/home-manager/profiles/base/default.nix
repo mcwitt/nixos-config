c7282ec9c15616e3b421fe5cad44cc21fdd93766
Matt Wittmann <mcwitt@gmail.com>
2025-04-23 09:12:33 -0700

    Update flake.lock
    Fix deprecation

--- modules/common/home-manager/profiles/base/default.nix
+++ modules/common/home-manager/profiles/base/default.nix
@@ -183,7 +183,7 @@
         extended = true;
       };
 
-      initExtra = ''
+      initContent = ''
         setopt HIST_IGNORE_SPACE
         source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
         source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
