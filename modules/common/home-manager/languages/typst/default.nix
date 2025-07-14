76dd97b5b73965deb1cda91ddea0d9fa2eb74c34
Matt Wittmann <mcwitt@gmail.com>
2025-07-14 09:36:01 -0700

    emacs: add mode to format typst files on save

--- modules/common/home-manager/languages/typst/default.nix
+++ modules/common/home-manager/languages/typst/default.nix
@@ -12,6 +12,22 @@ in
   options.languages.typst.enable = mkEnableOption "Typst language environment";
 
   config = mkIf cfg.enable {
-    programs.emacs.init.usePackage.typst-ts-mode.enable = true;
+
+    home.packages = [ pkgs.typstfmt ];
+
+    programs.emacs.init.usePackage = {
+      typst-ts-mode.enable = true;
+
+      reformatter = {
+        enable = true;
+        command = [ "typstfmt-on-save-mode" ];
+        config = ''
+          (reformatter-define typstfmt
+            :program "typstfmt"
+            :args nil
+            :lighter " TypstFmt")
+        '';
+      };
+    };
   };
 }
