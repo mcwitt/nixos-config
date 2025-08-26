e5f10d66ddfc2da597981eefcc60daf2830b12ae
Matt Wittmann <mcwitt@gmail.com>
2025-08-26 10:52:49 -0700

    home-assistant-custom-components.bhyve: 3.2.4 -> 3.2.5

--- packages/servers/home-assistant/custom-components/bhyve.nix
+++ packages/servers/home-assistant/custom-components/bhyve.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent rec {
   owner = "sebr";
   domain = "bhyve";
-  version = "3.2.4";
+  version = "3.2.5";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "bhyve-home-assistant";
     rev = version;
-    hash = "sha256-g2lBUrABpKgwVJIU67mKmzfLJy2spb0w7WloIRJ8xsE=";
+    hash = "sha256-7FKZqGVskK8fI48BPOQGg4BpvC0zdVIjbIoKAk/KFIk=";
   };
 
   meta = with lib; {
