b930357e334a57ec7f49f32423d8ddc86b996b21
Matt Wittmann <mcwitt@gmail.com>
2025-11-13 20:58:06 -0800

    nerdify-font: parallelize patching

--- overlay/nerdify-font.nix
+++ overlay/nerdify-font.nix
@@ -4,14 +4,28 @@ stdenv.mkDerivation {
   name = "${font.name}-nerd-font-patched";
   src = font;
   nativeBuildInputs = [ nerd-font-patcher ];
+
   buildPhase = ''
+    runHook preBuild
+
     mkdir build
-    for f in share/fonts/truetype/*.ttf; do
-        nerd-font-patcher -c --out build $f
-    done
+
+    # Recursively find all .ttf files under share/fonts/truetype
+    # -print0 and -0 handle filenames with spaces or special chars
+    find share/fonts/truetype -name '*.ttf' -print0 | \
+      xargs -P $NIX_BUILD_CORES -n 1 -0 nerd-font-patcher -c --out build
+
+    runHook postBuild
   '';
+
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/share/fonts/truetype
-    cp -a build/* $out/share/fonts/truetype/
+    if [ -n "$(ls -A build)" ]; then
+      cp -a build/* $out/share/fonts/truetype/
+    fi
+
+    runHook postInstall
   '';
 }
