39cd0b116432a9bffb618b132d5ed8534e57e8ff
Matt Wittmann <mcwitt@gmail.com>
2026-03-02 09:25:18 -0800

    emacs/citar: remove native-comp packageQuickstart workaround

--- modules/common/home-manager/profiles/base/emacs/citar.nix
+++ modules/common/home-manager/profiles/base/emacs/citar.nix
@@ -2,21 +2,6 @@
 {
   config = lib.mkIf config.profiles.base.enable {
     programs.emacs.init = {
-      # HACK: workaround for native-comp of the package quickstart
-      # file failing with citar
-      #
-      #     Type citar-indicator missing from # typeof-types!
-      #
-      # We disable packageQuickstart and add (package-activate-all) to
-      # init.el. The latter is needed because the emacs-init module
-      # sets package-enable-at-startup to nil when packageQuickstart
-      # is false.
-      packageQuickstart = lib.mkForce false;
-
-      prelude = ''
-        (package-activate-all)
-      '';
-
       usePackage = {
         citar = {
           enable = true;
