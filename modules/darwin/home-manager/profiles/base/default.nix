e8e12725f8397d0aea30af3db5a5573a6925c529
Matt Wittmann <mcwitt@gmail.com>
2026-03-02 09:25:08 -0800

    darwin: remove resolved fish PATH ordering workaround
    nix-darwin#122 is fixed upstream as of nixpkgs 24.11.

--- modules/darwin/home-manager/profiles/base/default.nix
+++ modules/darwin/home-manager/profiles/base/default.nix
@@ -1,14 +1,4 @@
 { config, pkgs, ... }:
 {
-
-  # HACK fix for darwin/fish PATH ordering; see https://github.com/LnL7/nix-darwin/issues/122
-  programs.fish.shellInit = ''
-    for p in /run/current-system/sw/bin ${config.home.profileDirectory}/bin
-        if not contains $p $fish_user_paths
-            set -g fish_user_paths $p $fish_user_paths
-        end
-    end
-  '';
-
   programs.git.ignores = pkgs.gitignores "Global/macOS";
 }
