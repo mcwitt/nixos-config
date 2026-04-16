b77fcecac56e0b6a3d45cc348af3d22f4db678a8
Matt Wittmann <mcwitt@gmail.com>
2026-04-16 12:39:33 -0700

    desktop: add alsa-utils

--- modules/nixos/profiles/desktop/pipewire.nix
+++ modules/nixos/profiles/desktop/pipewire.nix
@@ -1,8 +1,15 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 # https://nixos.wiki/wiki/PipeWire
 # rtkit is optional but recommended
 {
   config = lib.mkIf config.profiles.desktop.enable {
+    environment.systemPackages = [ pkgs.alsa-utils ];
+
     security.rtkit.enable = true;
 
     services.pipewire = {
