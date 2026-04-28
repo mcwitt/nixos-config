495214a474caf0af0848cfd9bc7a32a169e669c9
Matt Wittmann <mwittmann@kisotechnology.com>
2026-04-28 15:04:52 -0700

    karakuri: drop blueman-applet from hm config

--- hosts/karakuri/home/default.nix
+++ hosts/karakuri/home/default.nix
@@ -14,8 +14,6 @@ in
 
   programs.spotify.package = pkgs.spotify.override { deviceScaleFactor = 2.0; };
 
-  services.blueman-applet.enable = true;
-
   services.polybar.settings = {
     "bar/main" = {
       dpi = lib.mkForce dpi; # workaround; auto-detection fails
