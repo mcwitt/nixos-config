0681417b54cb32cf63397938eb17594caa6ec4dc
Matt Wittmann <mcwitt@gmail.com>
2026-06-09 14:39:41 -0700

    stylix: increase base font size

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -80,10 +80,10 @@ in
       };
 
       sizes = {
-        applications = 9;
-        desktop = 9;
-        popups = 9;
-        terminal = 9;
+        applications = 10;
+        desktop = 10;
+        popups = 10;
+        terminal = 10;
       };
     };
   };
