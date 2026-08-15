b596e08a0c79c2622ed9dfca66fc0b331de8f781
Matt Wittmann <matt.wittmann@openathena.ai>
2026-08-15 14:00:09 -0700

    github-gitignore: dcc0fc7 -> 57286c3

--- packages/data/misc/github-gitignore.nix
+++ packages/data/misc/github-gitignore.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner = "github";
   repo = "gitignore";
-  rev = "dcc0fc7bc2b5ba480cf117ad1be31bafceeaff46";
-  hash = "sha256-6iCmlbxb27MGtHqQGj98LUnmK17QCdCYCwgIlyYC6MM=";
+  rev = "57286c3887203259752b747db94e6c3ad10ec53d";
+  hash = "sha256-9Gn7c0LSAgD7oaS7KFx5BbGux37UM74w0cQdAg730uM=";
 }
